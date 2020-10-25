f8c3491bc86f899cd418926b861a6765ed62102f
YouHao <youhao0809@163.com>
2020-10-25 15:55:25 +0800

    天气

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,4 +1,7 @@
 // pages/welcome/welcome.js
+// var DomParser = require('../../lib/dom-parser.js')
+import DomParser from '../../lib/dom-parser.js'
+var newdata=''
 Page({
 
   /**
@@ -26,6 +29,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.request({
+      url: 'https://restapi.amap.com/v3/weather/weatherInfo',
+      data:{
+        key:'1923ac4ac1f38fb9cfab97e58029b24b',
+        city:310100,
+        output:'XML'
+      },
+      success:(res)=>{
+        console.log(res.data.toString())
+        var xmlDom = new DomParser.DOMParser()
+        var parsedData = xmlDom.parseFromString(res.data.toString())
+        var city = parsedData.getElementsByTagName('city')[0].firstChild.nodeValue.toString()
+        var weather = parsedData.getElementsByTagName('weather')[0].firstChild.nodeValue.toString()
+        var temperature = parsedData.getElementsByTagName('temperature')[0].firstChild.nodeValue.toString()
+        var winddirection = parsedData.getElementsByTagName('winddirection')[0].firstChild.nodeValue.toString()
+        var windpower = parsedData.getElementsByTagName('windpower')[0].firstChild.nodeValue.toString()
+        var humidity = parsedData.getElementsByTagName('humidity')[0].firstChild.nodeValue.toString()
+        var reporttime = parsedData.getElementsByTagName('reporttime')[0].firstChild.nodeValue.toString()
+        var weatherData = '城市:'+city+'\n天气'+weather+'\n气温:'+temperature+'\n风向:'+winddirection+'\n风速:'+windpower+'\n湿度:'+humidity+'\n更新时间:'+reporttime
+        this.setData({
+          newdata:weatherData
+        })
+      }
+    })   
     console.log("这是Welcome，OnLoad")
   },
 
